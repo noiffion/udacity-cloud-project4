c05e82714242a1ad7c7078839043f93338bc0b79
David Kerekes <davkedav@gmail.com>
2020-09-30 19:48:21 +0200

    feat(backend): implement CreateTodo

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,41 @@
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
+import { TodoCreate, TodoItem } from '../../models/TodoItem.d';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const todoId = uuid.v4();
+  const newTodoData: TodoCreate = JSON.parse(event.body);
+
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
+  console.log(jwtToken);
+
+  const newTodo: TodoItem = {
+    todoId,
+    createdAt: new Date().toISOString(),
+    userId: uuid.v4(), // getUserId(jwtToken),
+    done: false,
+    ...newTodoData
+  };
+
+  await docClient
+    .put({
+      TableName: todosTable,
+      Item: newTodo
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ newTodo })
+  };
+};
