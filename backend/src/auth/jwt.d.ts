eb19e9ee1b215fc5c0c491b8724614e2a3bb292d
David Kerekes <davkedav@gmail.com>
2020-10-01 14:39:54 +0200

    refct: merge interfaces into jwt.d.ts

--- backend/src/auth/jwt.d.ts
+++ backend/src/auth/jwt.d.ts
@@ -1,9 +1,12 @@
-import { JwtPayload } from './JwtPayload';
 import { JwtHeader } from 'jsonwebtoken';
 
-/**
- * Interface representing a JWT token
- */
+export interface JwtPayload {
+  iss: string;
+  sub: string;
+  iat: number;
+  exp: number;
+}
+
 export interface Jwt {
   header: JwtHeader;
   payload: JwtPayload;
