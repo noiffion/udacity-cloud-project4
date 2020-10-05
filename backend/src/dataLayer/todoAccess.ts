f26c7ee8441a66cca9e862a44aa133bf04e2c216
David Kerekes <davkedav@gmail.com>
2020-10-05 09:35:22 +0200

    feat(backend): implement distributed tracing (X-Ray)

---
+++ backend/src/dataLayer/todoAccess.ts
@@ -0,0 +1,43 @@
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { TodoItem } from '../models/Todo.d'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const logger = createLogger('todoAccess');
+
+export class TodoAccess {
+
+  constructor(
+    private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+    private readonly todosTable = process.env.TODOS_TABLE)
+    {
+  }
+
+  async getTodos(userId): Promise<TodoItem[]> {
+    logger.info('Getting all todo items')
+
+    const result = await this.docClient
+      .query({
+        TableName: this.todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+          ':userId': userId
+        }
+      })
+      .promise();
+    const todoList = result.Items;
+    logger.info('Successfully retrieved todolist');
+
+  }
+
+  async getOneTodo(group: Group): Promise<Group> {
+    await this.docClient.put({
+      TableName: this.groupsTable,
+      Item: group
+    }).promise()
+
+    return group
+  }
+}
